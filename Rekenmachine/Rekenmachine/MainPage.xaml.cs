ac5bfb2cefcab30554046eabbc80fc834a07a667
Sam Elfring <0318893@student.rocvantwente.nl>
2019-12-03 12:33:15 +0100

    error fix

--- Rekenmachine/Rekenmachine/MainPage.xaml.cs
+++ Rekenmachine/Rekenmachine/MainPage.xaml.cs
@@ -234,20 +234,12 @@ namespace Rekenmachine
         }
         private void PlusMinClick(object sender, RoutedEventArgs e) //Negatief/Positief Knop
         {
-<<<<<<< HEAD
             if (Uitkomst.Text != "") {
             double plusMin = Convert.ToDouble(invoer);
             plusMin -= plusMin * 2;
             invoer = Convert.ToString(plusMin);
             Uitkomst.Text = invoer;
-=======
-            if (invoer != string.Empty)
-            {
-                double plusMin = Convert.ToDouble(invoer);
-                plusMin -= plusMin * 2;
-                invoer = Convert.ToString(plusMin);
-                Uitkomst.Text = invoer;
->>>>>>> 339f4a838e15a2568b3ecb6e4ab034871aa0612b
+
             }
             else
             {
