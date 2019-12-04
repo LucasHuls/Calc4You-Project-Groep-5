2915e52733d0e36823a8dbf1cb7288910887955c
Sam Elfring <0318893@student.rocvantwente.nl>
2019-12-04 10:01:53 +0100

    Nieuw design in de README gezet

--- Rekenmachine/Rekenmachine/MainPage.xaml.cs
+++ Rekenmachine/Rekenmachine/MainPage.xaml.cs
@@ -21,9 +21,6 @@ using Windows.UI.ViewManagement;
 
 namespace Rekenmachine
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
